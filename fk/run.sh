3c115d533964ad394e2ba3cc3d68d9ca14fa715e
xiaoquanjie <404558965@qq.com>
2018-09-26 03:48:39 +0000

    youhua

--- fk/run.sh
+++ fk/run.sh
@@ -4,9 +4,18 @@ root_path=`pwd`
 #export LD_LIBRARY_PATH=/usr/local/lib
 #echo library path: ${root_path}/protobuflib/linux
 
-./blade-bin/routersvr/routersvr -D --conf_dir ${root_path}/conf/
-echo 'starting routersvr..........'
+echo "root_path:${root_path}"
 
-./blade-bin/connsvr/connsvr -D --conf_dir ${root_path}/conf/
-echo 'starting connsvr..........'
+svr_array=(routersvr connsvr)
+
+if [ $# == 1 ];then
+   echo "starting $1.........."
+   ./blade-bin/$1/$1 -D --conf_dir ${root_path}/conf/
+else
+   for item in ${svr_array[@]}
+   do
+     echo "starting $item.........."
+     ./blade-bin/$item/$item -D --conf_dir ${root_path}/conf/
+   done
+fi
 
