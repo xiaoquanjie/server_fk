c42a07f7989a096d0b7902f6984bc0c912f20469
xiaoquanjie <404558965@qq.com>
2018-10-20 12:52:21 +0800

    tmp

--- fk/dbtool/mysql_executor.cpp
+++ fk/dbtool/mysql_executor.cpp
@@ -5,15 +5,12 @@
 int MysqlExecutor::Execute(const dbtool::MysqlSchemaConf& cfg) {
 	for (int idx_schema = 0; idx_schema < cfg.mysql_schemas_size(); ++idx_schema) {
 		auto& schema = cfg.mysql_schemas(idx_schema);
-
-		MYSQL _st_mysql;
-		mysql_init(&_st_mysql);
-		/*SqlConnectionPtr sql_conn = MysqlPool::GetConnection(schema.mysql_ip().c_str(), schema.mysql_user().c_str(),
+		SqlConnectionPtr sql_conn = MysqlPool::GetConnection(schema.mysql_ip().c_str(), schema.mysql_user().c_str(),
 			schema.mysql_passwd().c_str(), "", schema.mysql_port());
 		if (!sql_conn) {
 			LogError("connect mysql fail: " << schema.mysql_ip());
 			return false;
-		}*/
+		}
 	}
 	return true;
 }
