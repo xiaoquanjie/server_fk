f73106173cbbccb957660aff103261be8845cf70
xiaoquanjie <404558965@qq.com>
2018-10-24 17:00:13 +0800

    tmp

--- fk/protoc/gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./protoc  --proto_path=../protolib/proto --cpp_out=../protolib/src/ routersvr_config.proto
-./protoc  --proto_path=../protolib/proto --cpp_out=../protolib/src/ cmd.proto
-./protoc  --proto_path=../protolib/proto --cpp_out=../protolib/src/ comm_conf.proto
-./protoc  --proto_path=../protolib/proto --cpp_out=../protolib/src/ router.proto
-./protoc  --proto_path=../protolib/proto --cpp_out=../protolib/src/ svr_base.proto
